8d9e7f12820d568a139c72418f62bfcf70d3c264
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-06-15 20:27:36 +0200

    1

---
+++ contacts.js
@@ -0,0 +1,21 @@
+/*
+ * Skomentuj i zapisz wartość
+ * const contactsPath = ;
+ */
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
